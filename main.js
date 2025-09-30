58b775e687d7a8202ca6b3d29467941a77266293
Mayahlam <141934499+Mayahlam@users.noreply.github.com>
2025-09-30 16:01:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function changeBgRed() {
+    const firstColor = document.getElementById("colorDisplay");
+    firstColor.style.backgroundColor = 'Red';
+}
+function changeBgOrange() {
+    const secColor = document.getElementById("colorDisplay");
+    secColor.style.backgroundColor = 'Orange'
+}
+function changeBgYellow() {
+    const thirdColor = document.getElementById("colorDisplay");
+    thirdColor.style.backgroundColor = 'Yellow'
+}
+function changeBgGreen() {
+    const fourthColor = document.getElementById("colorDisplay");
+    fourthColor.style.backgroundColor = 'Green'
+}
+function changeBgBlue() {
+    const fifthColor = document.getElementById("colorDisplay");
+    fifthColor.style.backgroundColor = 'Blue'
+}
+function changeBgIndigo() {
+    const sixthColor = document.getElementById("colorDisplay");
+    sixthColor.style.backgroundColor = 'Indigo'
+}
+function changeBgViolet() {
+    const lastColor = document.getElementById("colorDisplay");
+    lastColor.style.backgroundColor = 'Violet'
+}
